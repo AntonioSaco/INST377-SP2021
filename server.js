1fc5cc863b969dfe7c5273c91409aea36bd3b8a1
TonySaco <tony.saco@yahoo.com>
2021-02-19 02:13:03 -0500

    w4 final(?) update

--- server.js
+++ server.js
@@ -44,7 +44,8 @@ app.route('/api')
     console.log('Form data in res.body', req.body);
     console.log('Now send something back to your client');
     //res.json({data: dataToSendToFrontEnd});
-    res.send({data: req.body});
+    //res.send({data: req.body});//
+    res.send('Hello World').status(200);
     res.json(json);
   });
 
